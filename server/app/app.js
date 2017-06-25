ba9dc0c55ea4aa1c606333c9ce03eb96e975f723
Himanshu Singh <himanshuakatalli@live.com>
2017-06-25 11:25:05 +0530

    fixed

--- server/app/app.js
+++ server/app/app.js
@@ -11,7 +11,7 @@ var bodyParser = require('body-parser');
 mongoose.connect('mongodb://127.0.0.1/myapp');
 mongoose.Promise = global.Promise;
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extented : true }));
+app.use(bodyParser.urlencoded({ extended : true }));
 
 console.log('1 entry');
 mongoose.model('users',{name : String});
